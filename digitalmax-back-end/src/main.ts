03205457fa33c0b9d7d421e35531b9b721430b8e
Alexis Castillo <USTR007724@ugb.edu.sv>
2025-12-08 11:38:28 -0600

    Prueba

--- digitalmax-back-end/src/main.ts
+++ digitalmax-back-end/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   
   app.use(cookieParser());
   app.enableCors({
-  origin: "http://localhost:5173", 
+  origin: "http://DROPLET_IP:5173", 
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   allowedHeaders: "Content-Type, Authorization",
